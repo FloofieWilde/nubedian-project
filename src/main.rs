bf668ee080fb0affbcba2260e48e472b84372558
Floofie <61843570+PrinceLucario@users.noreply.github.com>
2022-08-06 01:38:55 +0200

    +page details

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #[macro_use] extern crate rocket;
 //#[macro_use] extern crate sqlx;
 
+use std::process::id;
+
 use rocket::response::content::{RawJson, self};
 use rocket_dyn_templates::Template;
 
@@ -38,6 +40,34 @@ async fn list(mut db: Connection<Nubedian>) -> content::RawHtml<Template> {
     .unwrap();
 
     rocket::response::content::RawHtml(Template::render("cpu_list", result))
+    
+}
+
+#[get("/<id>")]
+async fn Only_one(mut db: Connection<Nubedian>, id: &str) -> content::RawHtml<Template> {
+    let rq = "SELECT * FROM cpu_list WHERE id = ".to_owned()+id;
+    let q = sqlx::query(&rq);
+
+    let result: Vec<CpuList> = q
+    .map(|r: MySqlRow| CpuList{
+      ID: r.get("ID"), 
+      Price: r.get("Price"), 
+      CPUMark: r.get("CPUMark"), 
+      Name: r.get("Name"), 
+      Platform: r.get("Platform"), 
+      Socket: r.get("Socket"), 
+      Clockspeed: r.get("Clockspeed"), 
+      Turbospeed: r.get("Turbospeed"), 
+      Cores: r.get("Cores"), 
+      Threads: r.get("Threads"), 
+      TDP: r.get("TDP"), 
+      ReleaseDate: r.get("ReleaseDate")
+    })
+    .fetch_all(&mut *db)
+    .await
+    .unwrap();
+
+    rocket::response::content::RawHtml(Template::render("cpu_details", result))
     
 }
 
@@ -93,7 +123,7 @@ fn rocket() -> _ {
    rocket::build()
     .attach(Template::fairing())
     .attach(Nubedian::init())
-    .mount("/", routes![testjson, posttest,list])
+    .mount("/", routes![testjson, posttest, list, Only_one])
 }
 
 /// I intentionnaly didn't respected snake case for the variables to match names with the database
